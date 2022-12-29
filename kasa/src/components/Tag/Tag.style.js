db9136cfad13d645f7e4949afaf712f595376f01
Thanystos <guichardanthony@live.fr>
2022-12-29 11:09:59 +0100

    - Création du composant Tag - Déclaration de la page Housing

---
+++ kasa/src/components/Tag/Tag.style.js
@@ -0,0 +1,27 @@
+import styled from "@magister_zito/vue3-styled-components";
+import colors from "../../utils/colors";
+
+export const TagContainer = styled.div`
+    width: 115px;
+    height: 25px;
+    border-radius: 10px;
+    margin-right: 10px;
+    background-color: #${colors.primary};
+    color: white;
+    text-align: center;
+    line-height: 25px;
+
+    @media screen and (max-width: 635px) {
+        width: 84px;
+        height: 18px;
+        line-height: 18px;
+    }
+`
+
+export const TagTitle = styled.h3`
+    font-size: 14px;
+
+    @media screen and (max-width: 635px) {
+        font-size: 10px;
+    }
+`
---
+++ kasa/src/components/Tag/Tag.vue
@@ -0,0 +1,18 @@
+<template>
+    <TagContainer>
+        <TagTitle>
+            {{ title }}
+        </TagTitle>
+    </TagContainer>
+</template>
+
+<script setup>
+    import { TagContainer, TagTitle } from './Tag.style';
+
+    const props = defineProps({
+        title: {
+            type: String,
+            default: 'Non spécifié'
+        }
+    })
+</script>
